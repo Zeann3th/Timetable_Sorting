26132e6d226d42d629f3a720274f85404d749e46
Hien Long <longhienly112@gmail.com>
2024-02-05 10:52:22 +0700

    Fix some functions

--- temporary_test/test_functions.py
+++ temporary_test/test_functions.py
@@ -1,6 +1,7 @@
 import Timetable_Functions as Tf
 import warnings
 import numpy as np
+import copy
 from tkinter.filedialog import askopenfilename
 warnings.filterwarnings("ignore")
 
@@ -17,25 +18,48 @@ maHPs = Tf.Subject_filtering(filtered_data)
 
 
 def Try(k: int, dataframe, ma_hps: dict, _calendar=np.zeros((9, 1801)), _calendar_state=[], _initial_solution=[]):
-    key = list(ma_hps.keys())[k]
-    for maHP_items in ma_hps[key]:
+    """
+    Finding the initial solution for the problem
+    Arguments:
+        k: int
+        ma_hps: dictionary that have keys as subject's ID and values as subject's classes
+        _calendar: list that have weekdays from 2 to 8 and time from 0645 to 1800 (just use the default)
+        _calendar_state: empty list to store calendar
+        _initial_solution: empty list to store suitable classes
+    Returns:
+        Initial_solution: a list of suitable classes
+    """
+    key = list(ma_hps.keys())[k]  # Chọn mã học phần đã nhập (với k khác nhau thì là mã HP khác nhau)
+    for maHP_items in ma_hps[key]:  # Với mỗi mã lớp của mã học phần đó
         if Check(dataframe, _calendar, maHP_items):
-            _calendar_state.append(_calendar)
-            _initial_solution.append(maHP_items)
-            if k == len(list(ma_hps.keys()))-1:
-                if not _initial_solution:
+            # Kiểm tra xem lịch ở thời điểm đó có trống hay ko, trống thì true và điền vào lịch, đầy thì false
+            _calendar_state.append(_calendar)  # Lưu lại lịch cũ để khi backtrack còn quay lại lịch cũ để tìm kqua mới
+            _initial_solution.append(maHP_items)  # Biến chứa các mã lớp hợp lệ, dùng để return
+            if k == len(list(ma_hps.keys()))-1:  # nếu k == số HP nhập thì ...
+                if not _initial_solution:  # nếu lời giải rỗng => ...
                     print("There is no suitable timetable right now")
-                    return []
-                else:
+                else:  # nếu lời giải ko rỗng => ... (Cần phải exit chỗ này)
                     return _initial_solution
-            else:
-                Try(k + 1, dataframe, ma_hps, _calendar, _calendar_state, _initial_solution)
-                _initial_solution.pop()
-                _calendar_state.pop()
-                _calendar = _calendar_state[-1]
+            else:  # nếu k != số HP nhập, tìm mã lớp HP tiếp theo
+                result = Try(k + 1, dataframe, ma_hps, _calendar, _calendar_state, _initial_solution)
+                if result:
+                    return result
+                _initial_solution.pop()  # Nếu duyệt hết mà ko thấy mã lớp do kín lịch, backtrack và xóa mã lớp đã điền
+                _calendar_state.pop()  # Xóa lịch ko hợp lệ
+                _calendar = copy.deepcopy(_calendar_state[-1])  # copy trạng thái cũ của lịch
 
 
 def Check(dataframe, _calendar, class_id):
+    """"
+    Check if a class is suitable for the timetable
+    Arguments:
+        dataframe: filtered_data
+        _calendar: list that have weekdays from 2 to 8 and time from 0645 to 1800 (just use the default)
+        class_id: ID of classes. e.g: `151128`
+    Returns:
+        True if it is safe to fill in that class
+        False if it is not
+    """
     row = dataframe[dataframe["Mã lớp"] == class_id]
     date = int(row["Thứ"].values[0])
     start = int(row["Bắt đầu"].values[0])
